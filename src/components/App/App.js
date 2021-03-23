33adb71e956abf50e24830bab748fd23f32bd900
Alauddin Al Azad <saimon.azad@hotmail.com>
2021-03-23 14:48:58 +0600

    basic layout with material-ui

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from '../Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
